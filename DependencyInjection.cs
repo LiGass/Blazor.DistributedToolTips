a83abac564d25abc7923d30e2830d831b9c15749
Lilian Gassmann <lilian.gassmann@outlook.fr>
2022-03-24 18:19:13 +0100

    Add project files.

---
+++ DependencyInjection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ToolTipComponent.Models;
+
+namespace ToolTipComponent
+{
+	public static class DependencyInjection
+	{
+		public static void AddBlazorToolTips(this IServiceCollection services)
+		{
+			services.AddSingleton<IToolTipState, ToolTipState>();
+		}
+	}
+}
---
+++ Models/ToolTipState.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ToolTipComponent.Models
+{
+	public interface IToolTipState : IDisposable
+	{
+		event Action OnChange;
+		void NotifyStateChanged();
+		void ActivateToolTip();
+		int IncrementAnchorsCount();
+		bool IsActive { get; }
+	}
+	public class ToolTipState : IToolTipState
+	{
+		protected bool _toolTipsActive = false;
+		public event Action OnChange;
+		public void NotifyStateChanged() => OnChange?.Invoke();
+		protected ToolTipDisplayType _displayType;
+		private readonly NavigationManager _navigationManager;
+		private int anchorsCount;
+		public ToolTipState(NavigationManager navigationManager)
+		{
+			_navigationManager = navigationManager;
+			anchorsCount = 0;
+			_navigationManager.LocationChanged += NewPage;
+		}
+
+		public void Dispose()
+		{
+			_navigationManager.LocationChanged -= NewPage;
+		}
+		protected void NewPage(object sender, LocationChangedEventArgs e)
+		{
+			anchorsCount = 0;
+			_toolTipsActive = false;
+		}
+
+		public bool IsActive { get => _toolTipsActive; }
+		public void ActivateToolTip()
+		{
+			_toolTipsActive = !_toolTipsActive;
+			NotifyStateChanged();
+		}
+		public int IncrementAnchorsCount()
+		{
+			anchorsCount++;
+			return anchorsCount;
+		}
+	}
+	public enum ToolTipDisplayType
+	{
+		Bubble,
+		Container,
+		Modal
+	}
+}
