305c3e81018bbecff6428d19c6340015640db717
Lilian Gassmann <lilian.gassmann@outlook.fr>
2022-03-30 16:42:48 +0200

    restructuring cont'd

--- samples/Sample.ToolTipsWASM/Program.cs
+++ samples/Sample.ToolTipsWASM/Program.cs
@@ -4,19 +4,19 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Sample.ToolTips.WASM;
+using ToolTips;
 
-
-namespace Sample.ToolTips
+namespace SampleWASM.ToolTips
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.Services.AddBlazorToolTips();
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            await builder.Build().RunAsync();
-        }
-    }
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
+			builder.Services.AddBlazorToolTips();
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			await builder.Build().RunAsync();
+		}
+	}
 }
